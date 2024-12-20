cfcfab5beb611fb3407cdb3dad6f358fdda2601a
Emrah Koksal <mikejohnson3505@gmail.com>
2024-12-20 15:41:24 +0300

    updated api test

--- src/test/java/com/hepsiBurada/step_definitions/ApiSteps.java
+++ src/test/java/com/hepsiBurada/step_definitions/ApiSteps.java
@@ -1,11 +1,14 @@
 package com.hepsiBurada.step_definitions;
 
+import com.hepsiBurada.utilities.ConfigurationReader;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.openqa.selenium.json.Json;
 import org.json.JSONObject;
+
 import static io.restassured.RestAssured.given;
 
 
@@ -15,31 +18,35 @@ public class ApiSteps {
 
     @When("Test Get Request")
     public void testGetRequest() {
+        int id = 2;
         response = given().accept(ContentType.JSON)
-                .get("/gen/clients/ada");
+                .get("pet/"+id);
 
         response.prettyPrint();
 
         System.out.println("response.statusCode() = " + response.statusCode());
-        Assert.assertEquals("response code does not match",200, response.statusCode());
+        Assert.assertEquals("response code does not match", 200, response.statusCode());
 
     }
 
     @When("Test Post Request")
     public void testPostRequest() {
 
+        int id = 111;
         JSONObject json = new JSONObject();
-        json.put("key1", "value1");
-        json.put("key2", 123);
-        json.put("key3", true);
+        json.put("petId", id);
+        json.put("name", "Max");
+        json.put("status", "available");
 
-        response = given().accept(ContentType.JSON)
-                .body(json)
-                .post("/gen/servers/spring");
+        response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(json.toString())
+                .post("pet");
 
         response.prettyPrint();
 
         System.out.println("response.statusCode() = " + response.statusCode());
-        Assert.assertEquals("response code does not match",200, response.statusCode());
+        Assert.assertEquals("response code does not match", 200, response.statusCode());
     }
 }
--- src/test/resources/features/api.feature
+++ src/test/resources/features/api.feature
@@ -2,11 +2,11 @@ Feature: Test API
   Agile Story: As a user, I should be able to GET and POST API calls so that I retrieve and send data
 
 
-  @api
+  @api @api1
   Scenario: Get Data from Api
     When Test Get Request
 
 
-  @api
+  @api @api2
   Scenario: Post Data to Api
     When Test Post Request
