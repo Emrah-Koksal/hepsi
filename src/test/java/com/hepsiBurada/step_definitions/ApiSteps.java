1becf4c8d59f959fedd275b188d5b6be19567da5
Emrah Koksal <mikejohnson3505@gmail.com>
2024-12-20 22:35:11 +0300

    updated api test

--- src/test/java/com/hepsiBurada/step_definitions/ApiSteps.java
+++ src/test/java/com/hepsiBurada/step_definitions/ApiSteps.java
@@ -1,9 +1,11 @@
 package com.hepsiBurada.step_definitions;
 
 import com.hepsiBurada.utilities.ConfigurationReader;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.openqa.selenium.json.Json;
@@ -15,17 +17,34 @@ import static io.restassured.RestAssured.given;
 public class ApiSteps {
 
     Response response;
-
-    @When("Test Get Request")
-    public void testGetRequest() {
-        int id = 2;
+    @When("Get the pet with the id of {int}")
+    public void getThePetWithTheIdOf(int id) {
         response = given().accept(ContentType.JSON)
                 .get("pet/"+id);
 
         response.prettyPrint();
+    }
 
+    @Then("Verify response status code as {int}")
+    public void verifyResponseStatusCodeAsStatusCode(int expectedStatusCode) {
         System.out.println("response.statusCode() = " + response.statusCode());
-        Assert.assertEquals("response code does not match", 200, response.statusCode());
+        Assert.assertEquals("response code does not match", expectedStatusCode, response.statusCode());
+    }
+
+    @Then("Assert API results with the info of Pet's {string} , {string}")
+    public void assertAPIResultsWithTheInfoOfPetS(String expectedName, String expectedStatus) {
+        JsonPath jsonPath = response.jsonPath();
+        String actualName = jsonPath.getString("name");
+        String actualStatus = jsonPath.getString("status");
+        System.out.println("actualName = " + actualName);
+        System.out.println("actualStatus = " + actualStatus);
+        Assert.assertEquals("name does not match",expectedName, actualName );
+        Assert.assertEquals("status does not match",expectedStatus, actualStatus );
+    }
+
+    @When("Test Get Request")
+    public void testGetRequest() {
+
 
     }
 
@@ -46,7 +65,68 @@ public class ApiSteps {
 
         response.prettyPrint();
 
-        System.out.println("response.statusCode() = " + response.statusCode());
-        Assert.assertEquals("response code does not match", 200, response.statusCode());
+    }
+
+
+    @When("Post the pet with the id of {int} {String} {String}")
+    public void postThePetWithTheIdOfId(int id, String name, String status) {
+        JSONObject json = new JSONObject();
+        json.put("petId", id);
+        json.put("name", "Max");
+        json.put("status", "available");
+
+        response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(json.toString())
+                .post("pet");
+
+        response.prettyPrint();
+
+    }
+
+
+    @When("Post the pet with the id of {int} {string}")
+    public void postThePetWithTheIdOf(Integer id, String name, String status) {
+        JSONObject json = new JSONObject();
+        json.put("petId", id);
+        json.put("name", "name");
+        json.put("status", status);
+
+        response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(json.toString())
+                .post("pet");
+
+        response.prettyPrint();
+    }
+
+    @Then("Verify response body {string} and {string}")
+    public void verifyResponseBodyAnd(String expectedType, String expectedMessage) {
+
+        JsonPath jsonPath = response.jsonPath();
+        String actualType = jsonPath.getString("type");
+        String actualMessage = jsonPath.getString("message");
+        System.out.println("actualtype = " + actualType);
+        System.out.println("actualmessage = " + actualMessage);
+        Assert.assertEquals("name does not match",expectedType, actualType );
+        Assert.assertEquals("status does not match",expectedMessage, actualMessage );
+    }
+
+    @When("Post the pet {int}, and name as {string} and status as {string}")
+    public void postThePetIdAndNameAsAndStatusAs(int id, String name, String status) {
+        JSONObject json = new JSONObject();
+        json.put("petId", id);
+        json.put("name", name);
+        json.put("status", status);
+
+        response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(json.toString())
+                .post("pet");
+
+        response.prettyPrint();
     }
 }
--- src/test/resources/features/api.feature
+++ src/test/resources/features/api.feature
@@ -1,12 +1,23 @@
 Feature: Test API
   Agile Story: As a user, I should be able to GET and POST API calls so that I retrieve and send data
 
-
   @api @api1
-  Scenario: Get Data from Api
-    When Test Get Request
+  Scenario Outline:
+    When Get the pet with the id of <id>
+    Then Verify response status code as <statusCode>
+    Then Assert API results with the info of Pet's "<name>" , "<status>"
+    Examples:
+      | id  | name     | status    | statusCode |
+      | 100 | Michelle | sold      | 200        |
+      | 101 | Harley   | available | 200        |
 
 
   @api @api2
-  Scenario: Post Data to Api
-    When Test Post Request
+  Scenario Outline:
+    When Post the pet <id>, and name as "<name>" and status as "<status>"
+    Then Verify response status code as <statusCode>
+    Then Verify response body "<type>" and "<message>"
+    Examples:
+      | id  | name     | status    | statusCode | type    | message |
+      | 121 | Michelle | sold      | 200        | unknown | 121     |
+      | 122 | Harley   | available | 200        | unknown | 122     |
